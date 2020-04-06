861350b573f7ca198d545e6fd2b1687d78a6379e
Tosin Akinyele <tosinakinyele@Tosins-MacBook-Pro.local>
2020-04-06 16:09:12 +0100

    chore: add travis

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-export DATABASE_URL='postgresql://tosinakinyele:psql@localhost:5432/capstone_db'
-export TEST_DB_URL='postgresql://tosinakinyele:psql@localhost:5432/capstone_test_db'
+export DATABASE_URL='postgresql://postgres:psql@localhost:5432/capstone_db'
+export TEST_DB_URL='postgresql://postgres:psql@localhost:5432/capstone_test_db'
 export ENV=''
 export FLASK_APP=app
 
